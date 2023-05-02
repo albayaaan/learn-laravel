975f1e1a911217402ce0d16ededae91e6a861483
albayaaan <albayaaan.z@gmail.com>
2023-05-02 21:23:49 +0700

    add PegawaiSeeder

---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class PegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = Faker::create('id_ID');
+
+        for ($i=0; $i < 10 ; $i++) {
+            DB::table('pegawai')->insert([
+                'pegawai_nama' => $faker->name(),
+                'pegawai_jabatan' => $faker->jobTitle(),
+                'pegawai_umur' => $faker->numberBetween(20,40),
+                'pegawai_alamat' => $faker->address(),
+            ]);
+        }
+    }
+}
