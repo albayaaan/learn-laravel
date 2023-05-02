493ebf3f41ec50453befcb44f5352493d8d2d68d
albayaaan <albayaaan.z@gmail.com>
2023-05-02 16:42:37 +0700

    add blog fitur

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    public function index()
+    {
+        $nama = "Tsaqib Abyan";
+        $matkuls = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+        return view('biodata', ['nama' => $nama, 'matkuls' => $matkuls]);
+    }
+}
