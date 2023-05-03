631b20dcaef6cb115a84dd8617bf27a063f81531
albayaaan <albayaaan.z@gmail.com>
2023-05-03 09:12:51 +0700

    add style bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,65 +3,70 @@
 
 <head>
     <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
-<style type="text/css">
-    .pagination li {
-        float: left;
-        list-style-type: none;
-        margin: 5px;
-    }
-</style>
-
 
 <body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-center">www.malasngoding.com</h2>
+                <h3>Data Pegawai</h3>
 
-    <h2>www.malasngoding.com</h2>
-    <h3>Data Pegawai</h3>
+                <p>Cari Data Pegawai :</p>
+                <form action="/pegawai/cari" method="GET" class="form-inline">
+                    <input class="form-control" type="text" name="cari" placeholder="Cari Pegawai .."
+                        value="{{ old('cari') }}">
+                    <input class="btn btn-primary ml-3" type="submit" value="CARI">
+                </form>
 
-    <p>Cari Data Pegawai :</p>
-    <form action="/pegawai/cari" method="GET">
-        <input type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old('cari') }}">
-        <input type="submit" value="CARI">
-    </form>
+                <br />
 
-    <br />
+                <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
 
-    <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+                <br />
+                <br />
 
-    <br />
-    <br />
+                <table border="1" class="table table-bordered">
+                    <tr>
+                        <th>Nama</th>
+                        <th>Jabatan</th>
+                        <th>Umur</th>
+                        <th>Alamat</th>
+                        <th>Opsi</th>
+                    </tr>
+                    @foreach ($pegawais as $pegawai)
+                        <tr>
+                            <td>{{ $pegawai->pegawai_nama }}</td>
+                            <td>{{ $pegawai->pegawai_jabatan }}</td>
+                            <td>{{ $pegawai->pegawai_umur }}</td>
+                            <td>{{ $pegawai->pegawai_alamat }}</td>
+                            <td>
+                                <a class="btn btn-warning btn-sm"
+                                    href="/pegawai/edit/{{ $pegawai->pegawai_id }}">Edit</a>
+                                |
+                                <a class="btn btn-danger btn-sm"
+                                    href="/pegawai/hapus/{{ $pegawai->pegawai_id }}">Hapus</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
 
-    <table border="1">
-        <tr>
-            <th>Nama</th>
-            <th>Jabatan</th>
-            <th>Umur</th>
-            <th>Alamat</th>
-            <th>Opsi</th>
-        </tr>
-        @foreach ($pegawais as $pegawai)
-            <tr>
-                <td>{{ $pegawai->pegawai_nama }}</td>
-                <td>{{ $pegawai->pegawai_jabatan }}</td>
-                <td>{{ $pegawai->pegawai_umur }}</td>
-                <td>{{ $pegawai->pegawai_alamat }}</td>
-                <td>
-                    <a href="/pegawai/edit/{{ $pegawai->pegawai_id }}">Edit</a>
-                    |
-                    <a href="/pegawai/hapus/{{ $pegawai->pegawai_id }}">Hapus</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-
-    <br />
-    Halaman : {{ $pegawais->currentPage() }} <br />
-    Jumlah Data : {{ $pegawais->total() }} <br />
-    Data Per Halaman : {{ $pegawais->perPage() }} <br />
+                <br />
+                Halaman : {{ $pegawais->currentPage() }} <br />
+                Jumlah Data : {{ $pegawais->total() }} <br />
+                Data Per Halaman : {{ $pegawais->perPage() }} <br />
 
 
-    {{ $pegawais->links() }}
-
+                {{ $pegawais->links('pagination::bootstrap-4') }}
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
